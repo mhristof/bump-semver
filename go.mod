2756ba51ffae71be106c60ecefbfaea2f3f84fe4
Mike Christof <mhristof@gmail.com>
2020-09-17 13:19:06 +0100

    return the correct list of tags

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/go-git/go-git/v5 v5.1.0
+	github.com/mhristof/germ v1.2.3
 	github.com/sirupsen/logrus v1.5.0
-	github.com/spf13/cobra v0.0.7
+	github.com/spf13/cobra v1.0.0
+	golang.org/x/mod v0.3.0
 )
--- tag/main.go
+++ tag/main.go
@@ -1,29 +1,58 @@
 package tag
 
 import (
+	"bytes"
 	"fmt"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/mhristof/semver/log"
+	"golang.org/x/mod/semver"
 )
+
+func eval(command string) []string {
+	parts := strings.Split(command, " ")
+	cmd := exec.Command(parts[0], parts[1:]...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	return strings.Split(strings.TrimSuffix(string(stdout.Bytes()), "\n"), "\n")
+}
+
+type BySemVer []string
+
+func (a BySemVer) Len() int           { return len(a) }
+func (a BySemVer) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a BySemVer) Less(i, j int) bool { return semver.Compare(a[i], a[j]) < 0 }
 
 func Get(path string) []string {
 	fmt.Println(fmt.Sprintf("path: %+v", path))
 
-	r, err := git.PlainOpen(path)
-	if err != nil {
-		panic(err)
+	var ret []string
+	for _, tag := range eval(fmt.Sprintf("git -C %s show-ref --tag", path)) {
+		parts := strings.Split(tag, " ")
+		vTag := filepath.Base(parts[1])
+
+		if !semver.IsValid(vTag) {
+			log.WithFields(log.Fields{
+				"vTag": vTag,
+			}).Debug("Not a semver, skipping")
+
+			continue
+		}
+		fmt.Println(fmt.Sprintf("vTag: %+v", vTag))
+
+		ret = append(ret, vTag)
+
 	}
 
-	tags, err := r.TagObjects()
-	if err != nil {
-		panic(err)
-	}
-
-	err = tags.ForEach(func(t *object.Tag) error {
-		fmt.Println(t)
-		return nil
-	})
-
-	return []string{}
+	sort.Sort(BySemVer(ret))
+	return ret
 }
